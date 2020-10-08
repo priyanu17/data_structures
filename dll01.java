e6e2998a4e5c170c1c1c570c00d6c845eb9f8cd3
Anu Priya <53366411+priyanu17@users.noreply.github.com>
2020-10-08 14:15:51 +0530

    new cm

---
+++ dll01.java
@@ -0,0 +1,52 @@
+public class dll01 {
+    public static void main(String args [] ){
+
+
+        System.out.println("\nCreate and traverse a manual Dll");
+
+        //create dll object
+        DLL dllObject = new DLL();
+
+        //create nodes
+        DLL.Node n1 = new DLL.Node(11);
+        DLL.Node n2 = new DLL.Node(22);
+        DLL.Node n3 = new DLL.Node(33);
+        DLL.Node n4 = new DLL.Node(44);
+        DLL.Node n5 = new DLL.Node(55);
+        DLL.Node n6 = new DLL.Node(66);
+
+        //create link to head
+
+        dllObject.head = n1;
+
+        //create next links
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+
+        //create previous links
+
+        n6.prev = n5;
+        n5.prev = n4;
+        n4.prev = n3;
+        n3.prev = n2;
+        n2.prev = n1;
+
+        System.out.println("\nTraversing the doubly linked list");
+
+        dllObject.printDll();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
--- doublyLinkedLists.md
+++ doublyLinkedLists.md
@@ -1,3 +1,10 @@
 doublyLinkedList01.java : Create and traverse through a manual dll
 
-doublylinkedList02.java : 
+doublylinkedList02.java : All insertions in a Dll
+
+doublylinkedList03.java : All deletions in a Dll
+
+doublyLinkedList04.java : Traverse the Dll in reverse order
+
+
+
