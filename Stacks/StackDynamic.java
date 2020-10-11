0543529e4b57989f2cd1257cba698083fab9bae0
Anu Priya <53366411+priyanu17@users.noreply.github.com>
2020-10-11 15:31:06 +0530

    cm

---
+++ Stacks/StackDynamic.java
@@ -0,0 +1,34 @@
+package Stacks;
+
+public class StackDynamic{
+
+    StackNode root;
+
+    static class StackNode{
+        int data ;
+        StackNode next;
+
+        StackNode(int val){
+            this.data = val;
+            this.next = null;
+        }
+    }
+
+    public void push(){
+
+    }
+    public int pop(){
+
+    }
+    public boolean isEmpty(){
+
+    }
+
+    public int peek(){
+
+    }
+
+    public void printStack(){
+
+    }
+}//end of class
